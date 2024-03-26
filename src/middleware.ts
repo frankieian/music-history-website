bcf2aadc2431cf213c14a4b2b2cadd37bb9ad8f2
Frankie Ian <ianfrankie98@gmail.com>
2024-03-26 21:21:45 +1100

    fix middleware block about

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,5 +29,5 @@ export async function middleware(request: NextRequest) {
 }
  
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.svg$|$).*)'],
+  matcher: ['/((?!api|about|_next/static|_next/image|.*\\.svg$|$).*)'],
 }
